6cd1fd01a93d0145f0a2782ecf884c268c8f4ed0
moishy friedman <moshehlf@gmail.com>
2023-08-27 11:31:41 +0300

    Shape

--- index.ts
+++ index.ts
@@ -1,27 +1,57 @@
 // את הקוד שלכם תכתבו כאן
 
 //  1. Rectangle - מלבן
-class Rectangle {
+// class Rectangle {
+//   width: number;
+//   height: number;
+
+//   constructor(width: number, height: number) {
+//     this.height = height;
+//     this.width = width;
+//   }
+
+//   getArea(): string {
+//     return this.height * this.width;
+//   }
+// }
+//  2. Square - ריבוע
+// class Square extends Rectangle {
+//   constructor(width: number) {
+//     super(width, width);
+//   }
+// }
+// 3. Shape - צורה
+class Shape {
+  getInfo(): string {
+    return "This is a Shape";
+  }
+}
+
+class Rectangle extends Shape {
   width: number;
   height: number;
 
   constructor(width: number, height: number) {
+    super();
     this.height = height;
     this.width = width;
   }
-
-  getArea() {
+  getArea(): number {
     return this.height * this.width;
   }
-}
-//  2. Square - ריבוע
-class Square extends Rectangle {
-  constructor(width: number) {
-    super(width, width);
+  GetInfo(): string {
+    return "This is a Rectangle";
   }
 }
-// 3. Shape - צורה
 
-// 4. Method Chaining
+class ColoredRectangle extends Rectangle {
+  color: string;
 
-// 5. Shape with draw method
+  constructor(width: number, height: number, color: string) {
+    super(width, height);
+    this.color = color;
+  }
+  getInfo(): string {
+    return `This is a Rectangle in ${this.color}`;
+  }
+}
