7f498c1680a5480b558e7ba578b99def90b5460a
moishy friedman <moshehlf@gmail.com>
2023-08-27 11:32:20 +0300

    Method Chaining

--- index.ts
+++ index.ts
@@ -21,12 +21,41 @@
 //   }
 // }
 // 3. Shape - צורה
-class Shape {
-  getInfo(): string {
-    return "This is a Shape";
-  }
-}
+// class Shape {
+//   getInfo(): string{
+//     return "This is a Shape";
+//   }
+// }
 
+// class Rectangle extends Shape {
+//   width: number;
+//   height: number;
+
+//   constructor(width: number, height: number) {
+//     super();
+//     this.height = height;
+//     this.width = width;
+//   }
+//   getArea(): number{
+//     return this.height * this.width;
+//   }
+//   GetInfo(): string{
+//     return "This is a Rectangle";
+//   }
+// }
+
+// class ColoredRectangle extends Rectangle {
+//   color: string;
+
+//   constructor(width: number, height: number, color: string) {
+//     super(width, height);
+//     this.color = color;
+//   }
+//   getInfo(): string{
+//     return `This is a Rectangle in ${this.color}`;
+//   }
+// }
+// 4. Method Chaining
 class Rectangle extends Shape {
   width: number;
   height: number;
@@ -42,16 +71,20 @@ class Rectangle extends Shape {
   GetInfo(): string {
     return "This is a Rectangle";
   }
-}
-
-class ColoredRectangle extends Rectangle {
-  color: string;
-
-  constructor(width: number, height: number, color: string) {
-    super(width, height);
-    this.color = color;
+  setScale(number: number): this {
+    this.height *= number;
+    this.width *= number;
+    return this;
   }
-  getInfo(): string {
-    return `This is a Rectangle in ${this.color}`;
+  static getNewRectangle(
+    rectangle1: Rectangle,
+    rectangle2: Rectangle
+  ): Rectangle {
+    let newRectangle = new Rectangle(
+      rectangle1.width + rectangle2.width,
+      rectangle1.height + rectangle2.height
+    );
+    return newRectangle;
   }
 }
+// 5. Shape with draw method
