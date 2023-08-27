35b0669710b2cd56e2f6b3348b1620b240bee510
moishy friedman <moshehlf@gmail.com>
2023-08-27 10:37:37 +0300

    Rectangle

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
 // את הקוד שלכם תכתבו כאן
 
 //  1. Rectangle - מלבן
+class Rectangle {
+  width: number;
+  height: number;
 
+  constructor(width: number, height: number) {
+    this.height = height;
+    this.width = width;
+  }
+
+  getArea() {
+    return this.height * this.width;
+  }
+}
 //  2. Square - ריבוע
 
 // 3. Shape - צורה
